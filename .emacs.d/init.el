06f660400cd6abd8230923316f2dcd282b31e6f6
Takumi Ueda <jp.takumi.ueda@gmail.com>
2017-12-25 00:03:05 +0900

    Remove unnecessary lines

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -139,14 +139,6 @@
 ;; Reverting
 (global-auto-revert-mode 1)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (monokai-theme helm git-gutter flycheck auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
