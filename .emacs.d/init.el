97200bba23ad68eca639474affac62ac9c514a2f
Takumi Ueda <jp.takumi.ueda@gmail.com>
2018-01-03 14:11:10 +0900

    M-p -> s-p

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -99,7 +99,7 @@
 (define-key global-map (kbd "C-;")     'helm-mini)
 (define-key global-map (kbd "M-x")     'helm-M-x)
 (define-key global-map (kbd "C-x C-f") 'helm-find-files)
-(define-key global-map (kbd "M-p")     'helm-projectile)
+(define-key global-map (kbd "s-p")     'helm-projectile)
 (define-key global-map (kbd "s-F")     'helm-projectile-ag)
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 
